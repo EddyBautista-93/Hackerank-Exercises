e25f0209585d21a865e1a4f3fde6728641bf9e34
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-03-01 12:26:12 -0600

    getting use to new setup so there will be typos

---
+++ Typescript/loops.js
@@ -0,0 +1,6 @@
+var loops = function (n) {
+    for (var i = 0; i >= n; i++) {
+        console.log(n * n);
+    }
+};
+console.log(loops(5));
---
+++ Typescript/loops.ts
@@ -0,0 +1,7 @@
+const loops = (n: number) => {
+    for(let i = 0 ; i >= n;i++){
+        console.log(n * n)
+    }
+}
+
+loops(5)

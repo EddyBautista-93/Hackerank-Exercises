38628ada4f08dd6bc4b320e9725ab4b5b87be989
EddyBautista <bautista.j.eddy@gmail.com>
2020-05-26 16:18:08 -0500

    added before typescript compiler

--- Javascript/plusMinus.js
+++ Javascript/plusMinus.js
@@ -62,7 +62,21 @@ function plusMinus(arr) {
                  
               return elem; 
          }); 
+         console.log((positives / len) || 0);
+         console.log((negatives / len) || 0);
+         console.log((zeros / len) || 0);      
+    
     } 
-
+  
 
 }
+
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
+// 0.500000 
+// 0.333333 
+// 0.166667 
+    plusMinus([1, 2, 3, -1, -2, -3, 0, 0]);
+// 0.375000 
+// 0.375000 
+// 0.250000 
